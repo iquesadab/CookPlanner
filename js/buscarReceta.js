b51c09e577a64ec8e001f12b358ac06849c0b5c9
IVANNIA VANESSA QUESADA BOGANTES <iquesadab@ucenfotec.ac.cr>
2026-04-16 15:50:34 -0600

    [new]: se implementan eventos javascript en filtros y boton ver receta en buscar receta

--- js/buscarReceta.js
+++ js/buscarReceta.js
@@ -1,6 +1,10 @@
 let btnBuscar = document.getElementById("Buscar");
+let btnCategorias = document.getElementById("Categorias");
+let btnDificultad = document.getElementById("Dificultad");
+let botonesverReceta = document.getElementsByClassName("verReceta");
 
 let txtBuscarReceta = document.getElementById("txtBuscarReceta");
+
 
 // Función Principal
 function buscarRetorno(){
@@ -42,4 +46,41 @@ function validarCamposVacios(){
     return error;
 }
 
+// Función para el boton de categorías
+function categoriasRetorno(){
+    Swal.fire({
+        title: "Información por Categorías",
+        text: "Mostrando resultados de recetas por categorías",
+        icon: "success",
+        confirmButtonText: "OK"
+    })
+}
+
+// Función para el boton de dificultad
+function dificultadRetorno(){
+    Swal.fire({
+        title: "Información por Dificultad",
+        text: "Mostrando resultados de recetas según su nivel de dificultad",
+        icon: "success",
+        confirmButtonText: "OK"
+    })
+}
+
+// Función para el boton de verReceta
+function verRecetaRetorno(){
+    Swal.fire({
+        title: "Receta seleccionada",
+        text: "Redirigiendo a la receta seleccionada",
+        icon: "success",
+        confirmButtonText: "OK"
+    })
+}
+
 btnBuscar.addEventListener("click", buscarRetorno);
+btnCategorias.addEventListener("click", categoriasRetorno);
+btnDificultad.addEventListener("click", dificultadRetorno);
+
+// Eventos botones verReceta
+for(let i = 0; i < botonesverReceta.length; i++){
+    botonesverReceta[i].addEventListener("click", verRecetaRetorno);
+}
